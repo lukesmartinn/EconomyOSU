eeb512c1e03d47f6016c7cd6b1a12e3c0acb67ee
Martin Lukeš <martin.lukes@prorocketeers.com>
2026-05-12 14:45:49 +0200

    Spring boot setup with PostgreSQL configuration

---
+++ backend/economyosu/src/main/java/cz/osu/economyosu/EconomyosuApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.economyosu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomyosuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomyosuApplication.class, args);
+	}
+
+}
---
+++ backend/economyosu/src/test/java/cz/osu/economyosu/EconomyosuApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.economyosu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomyosuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
